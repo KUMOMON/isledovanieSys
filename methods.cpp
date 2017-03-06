4d39a646bc5f04731f1a593b14363479e6b519a2
Alex <sascha.uchiha@yandex.ru>
2017-03-07 00:01:54 +0300

    Added defenition method: MxM

--- methods.cpp
+++ methods.cpp
@@ -36,10 +36,14 @@ double Ravnomernost(const matrix<int> &);
 
 //}
 
+
 matrix<int> MplusM(const matrix<int> & a, const matrix<int> & b)
 {
     matrix<int> rez;
     int N = a.size();
+
+
+    rez.reserve(N);
 
     for(indexer row=0;row<N;row++)
     {
@@ -48,4 +52,19 @@ matrix<int> MplusM(const matrix<int> & a, const matrix<int> & b)
             rez[row][cell]=a[row][cell] + b[row][cell];
     }
     return rez;
+}
+
+matrix<int> MxM(const matrix<int> & a, const matrix<int> & b)
+{
+
+    matrix<int> rez;
+
+    for(indexer row=0;row<N;row++)
+        rez[row].reserve(N);
+
+    //Спасибо MSDN
+    for(indexer row=0;row<N;row++)
+        for(indexer cell=0;cell<N;cell++)
+            for(indexer inner = 0; inner<N-1;inner++)
+                rez[row][cell]+=a[row][inner] + b[inner][cell];
 }
